379e4ce64e338c5aa2366c951a63158469f2ca01
udaySunkara <45877380+udaySunkara@users.noreply.github.com>
2018-12-14 19:37:58 +0530

    Initial project

---
+++ src/app/feedback-submit-status/feedback-submit-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-submit-status',
+  templateUrl: './feedback-submit-status.component.html',
+  styleUrls: ['./feedback-submit-status.component.scss']
+})
+export class FeedbackSubmitStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/feedback-wrapper/feedback-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-wrapper',
+  templateUrl: './feedback-wrapper.component.html',
+  styleUrls: ['./feedback-wrapper.component.scss']
+})
+export class FeedbackWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
